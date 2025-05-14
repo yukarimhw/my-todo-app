21d98bdadcee7bf0a5598bce5d9d4a43663573b5
yukari404 <0211chair@my.com>
2025-05-15 02:41:48 +0900

    main config

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/config/MethodSecurityConfig.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/config/MethodSecurityConfig.java
@@ -1,8 +1,17 @@
 package com.example.todo.config;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+/*
+古い記述方法
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MethodSecurityConfig {
+}
+*/
 
 //@PreAuthorizeを有効にする
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+@EnableMethodSecurity
 public class MethodSecurityConfig {
 }
