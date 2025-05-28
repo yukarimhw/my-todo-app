f1d9ec33a1da09de450391352dc90361be4458b6
yukarimhw <0211chair@gmail.com>
2025-05-28 13:09:37 +0900

    Update indexController.java

--- spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/indexController.java
+++ spring-boot-introduction-start-here/src/main/java/com/example/todo/controller/indexController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller("/")
 public class indexController {
 
-      // http:localhost:8080/ -> "Hello, World!"
-      // GET / -> "Hello World"
+
       @GetMapping
       public String index() {
           return "index";
